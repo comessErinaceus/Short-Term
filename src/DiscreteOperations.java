7df78a25b89ee1038eee5e7ad2e321132e562197
comessErinaceus <gibbsj1131@my.uwstout.edu>
2021-10-15 02:40:45 -0500

    The XOr Function is now fully operational at O(n^2) time

--- src/DiscreteOperations.java
+++ src/DiscreteOperations.java
@@ -1,24 +1,58 @@
-
+import java.util.*;
 public class DiscreteOperations {
 public void TestInheritance() {
 	System.out.println("Inheritance works!");
 }
 
-public void union() {
-	//this will be a function for finding and or operation of arrays.
-}
+	public void union() {
+		//this will be a function for finding and or operation of arrays.
+	}
+	
+	public void intersection() {
+		//this will be a function for finding only the parts of arrays that are the same in both.
+	}
 
-public void intersection() {
-	//this will be a function for finding only the parts of arrays that are the same in both.
-}
+	/*Description: This function will return an ArrayList in ascending order containing only the elements from each Array list that are not common between them.
+	 * Sorting runs in O(n) time and populating the result array list runs in O(n) time making O(2n) time.
+	 * Inputs: Two ArrayLists sorted in ascending order.
+	 * Output: One ArrayList sorted in ascending order containing only the members of each list that are not common between them
+	*/
+	public ArrayList<Integer> exclusiveOr(ArrayList<Integer> ListOne, ArrayList<Integer> ListTwo) {
+		ArrayList<Integer> result = new ArrayList<Integer>(1);
+		
 
-/*Description: This function will return an ArrayList in ascending order containing only the elements from each Array list that are not common between them.
- * Sorting runs in O(n) time and populating the result array list runs in O(n) time making O(2n) time.
- * Inputs: Two ArrayLists sorted in ascending order.
- * Output: One ArrayList sorted in ascending order containing only the members of each list that are not common between them
-*/
-public int[] exclusiveOr() {
-	int[] result = {};
-	return result;
-}
+		boolean isInOther = false;
+		int lastAdded = -1;
+		for(int i = 0; i < ListOne.size(); i++) {
+			for(int e = 0; e < ListTwo.size(); e++) {
+				if(ListOne.get(i) == ListTwo.get(e)) {
+					isInOther = true;
+				}
+				
+			}
+			if(!isInOther && ListOne.get(i) != lastAdded) {
+				result.add(ListOne.get(i));
+				lastAdded = ListOne.get(i);
+			}
+			isInOther = false;
+		}
+		
+		for(int i = 0; i < ListTwo.size(); i++) {
+			for(int e = 0; e < ListOne.size(); e++) {
+				if(ListTwo.get(i) == ListOne.get(e)) {
+					isInOther = true;
+				}
+				
+			}
+			if(!isInOther && ListTwo.get(i) != lastAdded) {
+				result.add(ListTwo.get(i));
+				lastAdded = ListTwo.get(i);
+			}
+			isInOther = false;
+		}
+		
+		return result;
+	}
+	
+	
 }
--- src/PsudoCodeTester.java
+++ src/PsudoCodeTester.java
@@ -1,9 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PsudoCodeTester extends DiscreteOperations {
 	public static void main(String[] args) {
 		System.out.println("Hello and welcome this is the testing code for my implementation of the current PsudoCode!");
 		
 		DiscreteOperations DO = new DiscreteOperations();
-		DO.TestInheritance();
+		
+		
+		//init first array
+		int arrayOne[] = {1, 3, 4, 5, 5, 7, 8};
+		ArrayList<Integer> ListOne = new ArrayList<Integer>(7);
+		for(int i = 0; i < 7; i ++) {
+			ListOne.add(arrayOne[i]);
+		}
+		
+		//init second array
+		int arrayTwo[] = {2, 2, 3, 5, 7};
+		ArrayList<Integer> ListTwo = new ArrayList<Integer>(6);
+		for(int i = 0; i < 5; i ++) {
+			ListTwo.add(arrayTwo[i]);
+		}
+		
+		ArrayList<Integer> a = DO.exclusiveOr(ListOne, ListTwo);
+		
+		
+		printArrayList(ListOne);
+		
+		//the array list we care about should be {1,2,4,8}
+		printArrayList(a);
+		
+	}
+	
+	//Description: printArrayList will print the array list on one line;
+	//Input: An ArrayList of integers is the input.
+	//Output: Static Void.
+	public static void printArrayList(ArrayList<Integer> arrayOne) {
+		String st = ", ";
+		
+		for(int i = 0; i < arrayOne.size(); i ++) {
+			// set the st char to empty if last element.
+			if(i == arrayOne.size() - 1){st = "";}
+			System.out.print(arrayOne.get(i)+ st);
+			
+		}
+		System.out.println("");
 	}
 }
