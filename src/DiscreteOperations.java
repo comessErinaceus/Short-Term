c32b6436c68050d6ef982f7c1fa6bd5780461e27
comessErinaceus <gibbsj1131@my.uwstout.edu>
2021-10-15 00:07:55 -0500

    Fully documented the exclusiveOr function

--- src/DiscreteOperations.java
+++ src/DiscreteOperations.java
@@ -3,4 +3,22 @@ public class DiscreteOperations {
 public void TestInheritance() {
 	System.out.println("Inheritance works!");
 }
+
+public void union() {
+	//this will be a function for finding and or operation of arrays.
+}
+
+public void intersection() {
+	//this will be a function for finding only the parts of arrays that are the same in both.
+}
+
+/*Description: This function will return an ArrayList in ascending order containing only the elements from each Array list that are not common between them.
+ * Sorting runs in O(n) time and populating the result array list runs in O(n) time making O(2n) time.
+ * Inputs: Two ArrayLists sorted in ascending order.
+ * Output: One ArrayList sorted in ascending order containing only the members of each list that are not common between them
+*/
+public int[] exclusiveOr() {
+	int[] result = {};
+	return result;
+}
 }
